946547a61181b62db6f2090263ce0e4d5f28a747
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2022-09-05 18:31:33 -0700

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[]args){
+
+        Student Hugo = new Student();
+
+        Hugo.surname = "Reyes";
+        Hugo.firstname = "Zachary Michael Hugo";
+        Hugo.middleInitial = 'A';
+        Hugo.dateOfBirth = "05/15/2004";
+        Hugo.studentNumber = 2022104914;
+        Hugo.studentEmailAddress = "reyesza@students.national-u.edu.ph";
+        Hugo.iAmAwesome = true;
+        Hugo.sayMyStudentNumber();
+        Hugo.sayMyStudentEmailAddress();
+        Hugo.sayMyIAmAwesome();
+        
+    }
+}
+   
